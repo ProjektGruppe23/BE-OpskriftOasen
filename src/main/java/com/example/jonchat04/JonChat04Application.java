29c7a9c967de6ae551e6b4eed5dd9d5221cb7e6a
Ulrik <ulri6390@stud.kea.dk>
2023-11-06 10:01:42 +0100

    fix

---
+++ src/main/java/com/example/jonchat04/JonChat04Application.java
@@ -0,0 +1,13 @@
+package com.example.jonchat04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonChat04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonChat04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jonchat04/JonChat04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jonchat04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonChat04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
