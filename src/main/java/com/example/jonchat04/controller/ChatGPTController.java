182a96e42f2db4d7bb10b0c284b1002c1dec7d27
Frederik Wessel <frederikglimsager@hotmail.com>
2023-11-07 10:03:28 +0100

    update controlleren så den virker

--- src/main/java/com/example/jonchat04/controller/ChatGPTController.java
+++ src/main/java/com/example/jonchat04/controller/ChatGPTController.java
@@ -5,9 +5,7 @@ import com.example.jonchat04.dto.ChatResponse;
 import com.example.jonchat04.dto.Choice;
 import com.example.jonchat04.dto.Message;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,7 +14,7 @@ import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class ChatGPTController {
 
     private final WebClient webClient;
@@ -26,28 +24,22 @@ public class ChatGPTController {
     }
 
     @GetMapping("/chat")
-    public String chatWithGPT(){
-        return "chat";
-    }
-
-
-    @GetMapping("/chat")
     public List<Choice> chatWithGPT(@RequestParam String message) {
         ChatRequest chatRequest = new ChatRequest(); //ChatRequest objekt har jeg dannet med https://www.jsonschema2pojo.org/ værktøj
         chatRequest.setModel("gpt-3.5-turbo"); //vælg rigtig model. se powerpoint
         List<Message> lstMessages = new ArrayList<>(); //en liste af messages med roller
-        lstMessages.add(new Message("system", "Respond as a unhelpful assistent."));
-        lstMessages.add(new Message("user", message));
+        lstMessages.add(new Message("system", "You are a helpful assistant."));
+        lstMessages.add(new Message("user", "Where is Copenhagen"));
         chatRequest.setMessages(lstMessages);
         chatRequest.setN(1); //n er antal svar fra chatgpt
         chatRequest.setTemperature(1); //jo højere jo mere fantasifuldt svar (se powerpoint)
-        chatRequest.setMaxTokens(300); //længde af svar
+        chatRequest.setMaxTokens(30); //længde af svar
         chatRequest.setStream(false); //stream = true, er for viderekomne, der kommer flere svar asynkront
         chatRequest.setPresencePenalty(1); //noget med ikke at gentage sig. se powerpoint
 
         ChatResponse response = webClient.post()
                 .contentType(MediaType.APPLICATION_JSON)
-                .headers(h -> h.setBearerAuth("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"))
+                .headers(h -> h.setBearerAuth("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"))
                 .bodyValue(chatRequest)
                 .retrieve()
                 .bodyToMono(ChatResponse.class)
